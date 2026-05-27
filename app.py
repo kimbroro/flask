cf2bb6d5a6a2518796680039ba1518a0771a5726
kimbroro <ggiriric@gmail.com>
2026-05-27 21:57:22 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("홈")
-    return render_template("index.html")
+    print("홈")
+    return render_template("index.html")
+
+@app.route("/calcu",methods=['POST'])
+def calcu():
+    num1=int(request.form["num1"])
+    num2=int(request.form["num2"])
+    return f"안녕 계산기{num1+num2}"
+
 
 @app.route("/aa")
 def aa():
-    print("aa")
-    return"aa"
+    print("aa")
+    return"aa"
 
 @app.route("/bb")
 def bb():
-    print("bb")
-    return"bb"
+    print("bb")
+    return"bb"
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(debug=True)
